7952d5d95309422812df422cf1e6165a915525d3
Kevin Fujimoto <fujimoto.kevin@gmail.com>
2020-09-22 09:56:39 -0700

    deploy to heroku

--- server.js
+++ server.js
@@ -14,10 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+require("./routes/htmlRoutes")(app)
+require("./routes/apiRoutes")(app)
 
 app.listen(PORT, function() {
     console.log("app is listening on " + PORT)
 })
-
-require("./routes/htmlRoutes")(app)
-require("./routes/apiRoutes")(app)
